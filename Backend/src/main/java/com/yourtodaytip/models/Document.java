c447c449766843f4ac10375138dea250fa2d34d8
Abdulhameed Osama <meedosama2001@gmail.com>
2024-05-09 05:39:11 +0300

    Created Document Database Model

---
+++ Backend/src/main/java/com/yourtodaytip/models/Document.java
@@ -0,0 +1,117 @@
+package com.yourtodaytip.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.util.Pair;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "documents")
+@ToString
+
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToMany
+    private List<User> editors;
+
+    @ManyToMany
+    private List<User> viewers;
+
+
+    // Constructors
+
+    public Document() {
+    }
+
+    public Document(int id, String title, String content, User author, List<User> editors, List<User> viewers) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.editors = editors;
+        this.viewers = viewers;
+    }
+
+    public Document(String title, String content, User author, List<User> editors, List<User> viewers) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.editors = editors;
+        this.viewers = viewers;
+    }
+
+    // Getter and Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<User> getEditors() {
+        return editors;
+    }
+
+    public void setEditors(List<User> editors) {
+        this.editors = editors;
+    }
+
+    public List<User> getViewers() {
+        return viewers;
+    }
+
+    public void setViewers(List<User> viewers) {
+        this.viewers = viewers;
+    }
+
+    // toString() method
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", editors=" + editors +
+                ", viewers=" + viewers +
+                '}';
+    }
+}
