821bde9176ca00d9d37171150e8e1b47facf0e9c
Youssef Saaid <youssefadel@apessmartsolutions.com>
2024-05-15 01:39:07 +0300

    backup

--- FrontEnd/src/sections/Document/edit-new-document-form.tsx
+++ FrontEnd/src/sections/Document/edit-new-document-form.tsx
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import 'quill/dist/quill.snow.css';
 import { useForm } from 'react-hook-form';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { useMemo, useEffect } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import FormProvider, { RHFTextField } from 'src/components/hook-form';
@@ -17,20 +17,107 @@ import { Card, CardHeader, CardContent } from '@mui/material';
 // routes
 import { useParams } from 'src/routes/hooks';
 
+import { useGetUser } from 'src/api/user';
+
 import Editor from 'src/components/editor';
 
 // ----------------------------------------------------------------------
+//
+const opsData = [
+  {
+    char: '',
+    pos: 0,
+    deleteFlag: false,
+    boldFlag: false,
+    italicFlag: false,
+  },
+  {
+    char: 'a',
+    pos: 500000.0003,
+    deleteFlag: false,
+    boldFlag: false,
+    italicFlag: false,
+  },
+  {
+    char: 's',
+    pos: 750000.0004499999,
+    deleteFlag: false,
+    boldFlag: false,
+    italicFlag: false,
+  },
+  {
+    char: 'd',
+    pos: 875000.0005249999,
+    deleteFlag: false,
+    boldFlag: false,
+    italicFlag: false,
+  },
+  {
+    char: 's',
+    pos: 937500.0005625,
+    deleteFlag: false,
+    boldFlag: false,
+    italicFlag: false,
+  },
+  {
+    char: 'v',
+    pos: 968750.00058125,
+    deleteFlag: false,
+    boldFlag: true,
+    italicFlag: false,
+  },
+  {
+    char: 'a',
+    pos: 984375.000590625,
+    deleteFlag: false,
+    boldFlag: true,
+    italicFlag: false,
+  },
+  {
+    char: '',
+    pos: 1000000,
+    deleteFlag: false,
+    boldFlag: true,
+    italicFlag: false,
+  },
+];
 
 type Props = {
   currentDocument?: { title: string; content: string } | null;
 };
-const Delta = Quill.import('delta');
-export default function DocsNewEditForm({ currentDocument }: Props) {
-  const params = useParams();
 
+const Delta = Quill.import('delta');
+
+const getDocFromOperations = (ops: any) => {
+  const opsDelta = new Delta();
+  const sortedData = [...ops].sort((a, b) => a.pos - b.pos);
+  sortedData.forEach((item) => {
+    opsDelta.insert(item.char, { bold: item.boldFlag, italic: item.italicFlag });
+  });
+  return opsDelta;
+};
+
+const ws = new WebSocket('ws://localhost:8080');
+
+export default function DocsNewEditForm({ currentDocument }: Props) {
+  const { user } = useGetUser();
+  console.log(user);
+
+  const [docData, setDocData] = useState();
+
+  useEffect(() => {
+    if (user) {
+      ws.send(JSON.stringify({ type: 'join', roomId: 'room1', userid: user.id }));
+      console.log('Connected to the websockets server');
+    }
+  }, [user]);
+
+  ws.onmessage = (event) => {};
+
+  const params = useParams();
   const { id } = params;
   const newsfeedId = parseInt(id!, 10);
-  console.log(newsfeedId);
+  // console.log(newsfeedId);
 
   const NewLocationSchema = Yup.object().shape({
     title: Yup.string().required('Please Enter Title'),
@@ -51,7 +138,6 @@ export default function DocsNewEditForm({ currentDocument }: Props) {
   });
 
   const { reset, setValue, handleSubmit } = methods;
-
   useEffect(() => {
     if (currentDocument) {
       reset(defaultValues);
@@ -61,6 +147,20 @@ export default function DocsNewEditForm({ currentDocument }: Props) {
   const onSubmit = handleSubmit(async (data: any) => {
     console.log(data);
   });
+
+  const onChangeDoc = (value, delta, source) => {
+    console.log(delta);
+  };
+
+  const onChangeDocCursure = (range, oldRange, source) => {
+    if (range) {
+      if (range.length === 0) {
+        console.log('User cursor is on', range.index);
+      }
+    } else {
+      console.log('Cursor not in the editor');
+    }
+  };
 
   return (
     <FormProvider methods={methods} onSubmit={onSubmit}>
@@ -72,25 +172,12 @@ export default function DocsNewEditForm({ currentDocument }: Props) {
           <CardContent>
             <Editor
               id="full-editor"
-              defaultValue={new Delta()
-                .insert('Some ')
-                .insert('initial', { bold: true })
-                .insert(' ')
-                .insert('content', { italic: true })
-                .insert('\n')}
+              defaultValue={getDocFromOperations(opsData)}
               onChange={(value, delta, source) => {
-                console.log(value);
-                console.log(delta);
-                console.log(source);
+                onChangeDoc(value, delta, source);
               }}
               onChangeSelection={(range, oldRange, source) => {
-                if (range) {
-                  if (range.length === 0) {
-                    console.log('User cursor is on', range.index);
-                  }
-                } else {
-                  console.log('Cursor not in the editor');
-                }
+                onChangeDocCursure(range, oldRange, source);
               }}
             />
           </CardContent>
